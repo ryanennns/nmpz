ba7d95fe6ca6ebaa4743ed0af2e107d16907c08f
Ryan Enns <ryanennns@gmail.com>
2026-02-25 20:16:19 -0500

    fix double event listener

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,38 +2,24 @@
 
 namespace App\Providers;
 
-use App\Events\RoundFinished;
-use App\Listeners\StartNextRound;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         $this->configureDefaults();
-
-        Event::listen(RoundFinished::class, StartNextRound::class);
     }
 
-    /**
-     * Configure default behaviors for production-ready applications.
-     */
     protected function configureDefaults(): void
     {
         Date::use(CarbonImmutable::class);
