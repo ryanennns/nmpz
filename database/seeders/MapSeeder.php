f4ae6f10d565494f845100a2a8d6020d0ef443d8
Ryan Enns <ryanennns@gmail.com>
2026-02-27 17:05:37 -0500

    map

--- database/seeders/MapSeeder.php
+++ database/seeders/MapSeeder.php
@@ -12,7 +12,7 @@ class MapSeeder extends Seeder
     public function run(): void
     {
         $mapName = 'likeacw-mapillary';
-        $filePath = base_path('likeacw-mapillary.jsonl');
+        $filePath = base_path('mapillary-balanced.jsonl');
 
         if (! file_exists($filePath)) {
             throw new \RuntimeException("Seed file not found: {$filePath}");
