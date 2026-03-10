40e43793a2235c94719132f16453c3d7f01f3af0
Ryan Enns <ryanennns@gmail.com>
2026-03-09 20:30:44 -0400

    oops

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Enums\GameStatus;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -41,15 +42,20 @@ class Player extends Model
         return $this->hasMany(Game::class, 'winner_id');
     }
 
-    public function games(): \Illuminate\Database\Eloquent\Builder
+    public function games(): Builder
     {
         return Game::where('player_one_id', $this->getKey())
             ->orWhere('player_two_id', $this->getKey());
     }
 
+    public function soloGames(): HasMany
+    {
+        return $this->hasMany(SoloGame::class);
+    }
+
     public function hasActiveGame(): bool
     {
-        return Game::query()
+        return $this->soloGames()
             ->where('status', GameStatus::InProgress)
             ->where(function ($query) {
                 $query->where('player_one_id', $this->getKey())
@@ -60,7 +66,7 @@ class Player extends Model
 
     public function highScore(): int
     {
-        return SoloGame::query()
+        return $this->soloGames()
             ->orderByDesc('score')
             ->select('score')
             ->first()?->score ?? 0;
